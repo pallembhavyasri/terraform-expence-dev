faaa620d79868f36ebcbfd49690b5e9f158a88f7
Bhavya Sri P <pallembhavya1@gmail.com>
2024-07-01 10:29:15 +0530

    terraform vpc module

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -3,3 +3,27 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.vpc-test.vpc_id
 }
+
+
+#["id1","id2"] - Terraform format
+# id1, id2 -- AWS SSM format -- Stringlist 
+# join() is the function which chnages from list to string list format 
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
+  type  = "StringList"
+  value = join("," ,module.vpc-test.public_subnet_ids)
+}
+
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
+  type  = "StringList"
+  value = join("," ,module.vpc-test.private_subnet_ids)
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/database_subnet_ids"
+  type  = "StringList"
+  value = join("," ,module.vpc-test.database_subnet_ids)
+}
