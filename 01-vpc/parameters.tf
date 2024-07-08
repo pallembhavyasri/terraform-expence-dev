7f3119b66e9333ba4b4ac1c5aedc8e1fdefbbb3b
Bhavya Sri P <pallembhavya1@gmail.com>
2024-07-08 18:32:10 +0530

    01 VPC module

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -27,3 +27,9 @@ resource "aws_ssm_parameter" "database_subnet_ids" {
   type  = "StringList"
   value = join("," ,module.vpc-test.database_subnet_ids)
 }
+
+resource "aws_ssm_parameter" "db_subnet_group_name" {
+  name  = "/${var.project_name}/${var.environment}/db_subnet_group_name"
+  type  = "String"
+  value = module.vpc-test.database_subnet_group_name
+}
